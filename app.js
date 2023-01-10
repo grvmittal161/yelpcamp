67bbe2f00b6982ba60aa2ba72969729f1772101b
grvmittal161 <grvmittal161@gmail.com>
2023-01-10 17:01:52 +0530

    all

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
+app.use(express.urlencoded({extended:true}))
+
 app.get('/',(req,res)=>{
     res.render('home')
 })
@@ -46,9 +48,18 @@ app.get('/save/campgrounds',async (req,res)=>{
 // const campgrounds= await Campground.find({});
 // res.render('campgrounds/index',{campgrounds})
 })
+app.get('/campgrounds/new', async (req,res)=>{
+  await  res.render('campgrounds/new');
+})
+
+app.post('/campgrounds',async (req,res)=>{
+   const campground=new Campground(req.body.campground);
+   await campground.save();
+   res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.get('/campgrounds/:id',async (req,res)=>{
-  await   connection();
+    connection();
   const campground=await  Campground.findById(req.params.id)
     res.render('campgrounds/show',{campground});
 })
